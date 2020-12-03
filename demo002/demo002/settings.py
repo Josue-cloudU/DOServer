f465a8ad8cfde21a55e9f2b3e1c7c56117e59679
Josue-cloudU <josue.avila17@tectijuana.edu.mx>
2020-12-03 12:14:40 -0800

    eliminar

--- demo002/demo002/settings.py
+++ demo002/demo002/settings.py
@@ -93,7 +93,7 @@ else:
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'pdemo002',
             'USER': 'Josue',
-            'PASSWORD': '123',
+            'PASSWORD': ''123'',
             'HOST': 'localhost',
             'PORT': '',
         }
