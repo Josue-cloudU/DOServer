c7d05f7dc02a91cc2bc0f93c23c2d7a6503fa9cc
Josue-cloudU <josue.avila17@tectijuana.edu.mx>
2020-12-03 10:56:13 -0800

    conf bd y statics

--- demo002/demo002/settings.py
+++ demo002/demo002/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'w3crv(jxkk-xiaucz-1abw-yf_wj+iuzr&o77!wjo#lne3^d7f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['165.22.183.231']
 
@@ -76,17 +76,28 @@ WSGI_APPLICATION = 'demo002.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_demo001',
-        'USER': 'user1',
-        'PASSWORD': '123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'db_demo001',
+            'USER': 'user1',
+            'PASSWORD': '123',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'pdemo002',
+            'USER': 'Josue',
+            'PASSWORD': '123',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
@@ -127,6 +138,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "statics",
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / "statics",
+#]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/')
