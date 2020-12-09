c446e3ff94926bc7d8f3a37c680f373783830756
Josue-cloudU <josue.avila17@tectijuana.edu.mx>
2020-12-09 10:54:21 -0800

    statics

--- demo002/demo002/settings.py
+++ demo002/demo002/settings.py
@@ -141,5 +141,5 @@ USE_TZ = True
 #    BASE_DIR / "statics",
 #]
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'statics')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'statics_root')
